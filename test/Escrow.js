bd04f0c43607695c6bb3eadf51f001baa271e233
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-10-31 05:23:32 +0530

    test_escrow: Started out with the test for escrow

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,5 +6,41 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    let buyer, seller, inspector, lender;
+    let realEstate; 
+    beforeEach(async() => { 
+        [buyer, seller, inspector, lender] = await ethers.getSigners();
+        const RealEstate = ethers.getContractFactory('RealEstate');
+        realEstate = await RealEstate.deployed();
 
+        let transaction = await realEstate.connect(seller).mint(buyer.address, tokens(1));
+
+        await transaction.wait(); 
+
+        const Escrow = await ethers.getContractFactory('Escrow');
+
+        escrow = await Escrow.deploy(realEstate.address, buyer.address, seller.address, inspector.address, lender.address);
+    })
+
+    describe('Deployment', () => { 
+        it('Returns NFT Address', async() => { 
+            let result = await escrow.nftAddress(); 
+            expect(result).to.equal(realEstate.address);
+        })
+
+        it('Returns seller', async() => { 
+            let result = await escrow.seller(); 
+            expect(result).to.equal(seller.address);
+        })
+
+        it('Returns inspector', async() => {     
+            let result = await escrow.inspector(); 
+            expect(result).to.equal(inspector.address);
+        })
+
+        it('Returns lender', async() => {     
+            let result = await escrow.lender(); 
+            expect(result).to.equal(lender.address);
+        })
+    })
 })
